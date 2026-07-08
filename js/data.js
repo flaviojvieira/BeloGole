f6d0ba46eee596763a781dc5202759c255b39a12
Flávio Vieira <jsv.flavio@gmail.com>
2026-07-07 21:13:42 -0300

    atualizando

--- js/data.js
+++ js/data.js
@@ -45,53 +45,10 @@ const PROMOCOES = [
 
 const PROMOCOES_EXIBIDAS = Array.from({ length: 8 }, (_, index) => PROMOCOES[index % PROMOCOES.length]);
 
-// Helper para criar um produto individual com nome, preço e imagem próprios
-function produto(img, nome, preco) {
-  return { img, nome, preco };
-}
-
 const CARDAPIO = [
   {
     categoria: 'Cerveja',
-    produtos: [
-      // Fardo
-      produto('assets/cervejas.png', 'Heineken - Fardo c/8', '40,00'),
-      produto('assets/cervejas.png', 'Original - Fardo c/8', '35,00'),
-      produto('assets/cervejas.png', 'Império - Fardo c/15', '40,00'),
-      produto('assets/cervejas.png', 'Skol - Fardo c/15', '45,00'),
-      produto('assets/cervejas.png', 'Itaipava - Fardo c/12', '35,00'),
-      // 600ml
-      produto('assets/cervejas.png', 'Heineken 600ml', '17,00'),
-      produto('assets/cervejas.png', 'Original 600ml', '14,00'),
-      produto('assets/cervejas.png', 'Império 600ml', '12,00'),
-      // Caçulinha 300ml
-      produto('assets/cervejas.png', 'Caçulinha Antarctica 300ml', '5,00'),
-      produto('assets/cervejas.png', 'Caçulinha Original 300ml', '6,00'),
-      // Balde com 10 - Caçulinha 300ml
-      produto('assets/cervejas.png', 'Balde Antarctica 300ml (10un)', '50,00'),
-      produto('assets/cervejas.png', 'Balde Original 300ml (10un)', '60,00'),
-      // 269ml
-      produto('assets/cervejas.png', 'Skol 269ml', '5,00'),
-      produto('assets/cervejas.png', 'Original 269ml', '5,00'),
-      produto('assets/cervejas.png', 'Império 269ml', '5,00'),
-      produto('assets/cervejas.png', 'Heineken 269ml', '6,00'),
-      // Long Neck
-      produto('assets/longneck.png', 'Long Neck Heineken', '12,00'),
-      produto('assets/longneck.png', 'Long Neck Corona', '12,00'),
-      produto('assets/longneck.png', 'Long Neck Budweiser', '10,00'),
-    ],
-  },
-  {
-    categoria: 'Drinks',
-    produtos: [
-      produto('assets/drinks.png', 'Caipirinha', '15,00'),
-      produto('assets/drinks.png', 'Nevada', '15,00'),
-      produto('assets/drinks.png', 'Gin Tônica', '20,00'),
-      produto('assets/drinks.png', 'Batida de Morango', '20,00'),
-      produto('assets/drinks.png', 'Batida de Maracujá', '20,00'),
-      produto('assets/drinks.png', 'Ice de Morango', '25,00'),
-      produto('assets/drinks.png', 'Ice de Kiwi', '25,00'),
-    ],
+    produtos: criarProdutos('assets/cervejas.png', 'Cerveja', '29,90'),
   },
   {
     categoria: 'Combo',
@@ -99,60 +56,18 @@ const CARDAPIO = [
   },
   {
     categoria: 'Whisky',
-    produtos: [
-      // Doses
-      produto('assets/wisky.png', 'Dose Red Label', '15,00'),
-      produto('assets/wisky.png', "Dose Jack Daniel's", '20,00'),
-      produto('assets/wisky.png', 'Dose Cavalo Branco', '15,00'),
-      produto('assets/wisky.png', 'Dose Black & White', '20,00'),
-      produto('assets/wisky.png', 'Dose Ballantines', '15,00'),
-      produto('assets/wisky.png', 'Dose Jameson', '20,00'),
-      produto('assets/wisky.png', 'Dose Grants', '20,00'),
-      // Garrafas
-      produto('assets/wisky.png', 'Garrafa Red Label', '140,00'),
-      produto('assets/wisky.png', 'Garrafa Cavalo Branco', '100,00'),
-      produto('assets/wisky.png', 'Garrafa Grants', '170,00'),
-      produto('assets/wisky.png', "Garrafa Jack Daniel's", '150,00'),
-      produto('assets/wisky.png', 'Garrafa Ballantines', '120,00'),
-      produto('assets/wisky.png', 'Garrafa Black & White', '140,00'),
-      produto('assets/wisky.png', 'Garrafa Jameson', '150,00'),
-    ],
+    produtos: criarProdutos('assets/wisky.png', 'Whisky', '89,90'),
+  },
+  {
+    categoria: 'Copão',
+    produtos: criarProdutos('assets/copao.png', 'Copão', '59,90'),
   },
   {
     categoria: 'Vodka',
-    produtos: [
-      produto('assets/vodka.png', 'Dose Smirnoff', '15,00'),
-      produto('assets/vodka.png', 'Dose Absolut', '20,00'),
-    ],
+    produtos: criarProdutos('assets/vodka.png', 'Vodka', '29,90'),
   },
   {
-    categoria: 'Cachaça',
-    produtos: [
-      produto('assets/copao.png', 'Velho Barreiro', '4,00'),
-      produto('assets/copao.png', 'Velho Barreiro com Limão', '5,00'),
-      produto('assets/copao.png', 'Cachaça 51', '5,00'),
-      produto('assets/copao.png', 'São Francisco', '6,00'),
-      produto('assets/copao.png', 'Ypioca Envelhecida', '15,00'),
-      produto('assets/copao.png', 'Dom Taparro Premium', '20,00'),
-      produto('assets/copao.png', 'Dom Taparro Premium no Carvalho', '25,00'),
-    ],
-  },
-  {
-    categoria: 'Vinho',
-    produtos: [
-      produto('assets/combo.png', 'Vinho Chalise', '17,00'),
-      produto('assets/combo.png', 'Vinho São Tomé', '25,00'),
-      produto('assets/combo.png', 'Vinho Pérgola', '28,00'),
-      produto('assets/combo.png', 'Vinho Cosecha', '45,00'),
-      produto('assets/combo.png', 'Vinho Alecrim', '45,00'),
-      produto('assets/combo.png', 'Vinho Pêra Negra', '45,00'),
-    ],
-  },
-  {
-    categoria: 'Licor',
-    produtos: [
-      produto('assets/campari.png', 'Dose Sabores de Licor', '10,00'),
-      produto('assets/campari.png', 'Garrafa Licores Dom Taparro', '80,00'),
-    ],
+    categoria: 'Drinks',
+    produtos: criarProdutos('assets/drinks.png', 'Drink', '119,90'),
   },
 ];
